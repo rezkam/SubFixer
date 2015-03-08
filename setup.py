00f08155e2939bdfbed8cdb026818ea011dc81c4
Mohammad Reza Kamalifard <mr.kamalifard@gmail.com>
2015-03-08 11:49:03 +0330

    update setup.py v0.2

--- setup.py
+++ setup.py
@@ -2,10 +2,13 @@ from distutils.core import setup
 
 setup(
     name='SubFixer',
-    version='0.1',
+    version='0.2',
     py_modules = ['subfixer'],
     license='GPL 3.0',
     author='Mohammad Reza Kamalifard',
+    install_requires=[
+        'click'
+    ],
     author_email='mrkamalifard@gmail.com',
     description='',
     entry_points = '''
